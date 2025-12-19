dcda594a412bdb7e288b6295e4d886c9a1c23e87
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-12-19 22:23:40 +0000

    more colors

--- src/yaam.c
+++ src/yaam.c
@@ -9,11 +9,11 @@ Copyright (c) 2025 Christmas_Missionary - BSD Zero Clause License
 /* clang-format off */
 #ifdef __func__
   #define ERR_STR \
-    "%s File: \33[38;5;51m%s\33[0m, \33[38;5;226mFunc not available\33[0m%s, Line: \33[38;5;51m%d\33[0m, Message: " \
+    "%s File: \33[38;5;46m%s\33[0m, \33[1m\33[38;5;201mFunc not available\33[0m%s, Line: \33[38;5;46m%d\33[0m, Message: " \
     "\33[38;5;51m%s\33[0m, Expression: \33[38;5;51m%s\33[0m\n"
 #else
   #define ERR_STR \
-    "%s File: \33[38;5;51m%s\33[0m, Func: \33[38;5;51m%s\33[0m, Line: \33[38;5;51m%d\33[0m, Message: " \
+    "%s File: \33[38;5;46m%s\33[0m, Func: \33[38;5;46m%s\33[0m, Line: \33[38;5;46m%d\33[0m, Message: " \
     "\33[38;5;51m%s\33[0m, Expression: \33[38;5;51m%s\33[0m\n"
 #endif
 /* clang-format on */
@@ -33,8 +33,8 @@ void yaam_warn_handler(const char * msg, const char * file, const char * fnc, in
 
 YAAM_NO_RET
 void yaam_err_handler(int type, const char * msg, const char * file, const char * fnc, int line, const char * expr) {
-  (void)printf(ERR_STR, (type) ? "\33[38;5;196mFatal Error!\33[0m" : "\33[38;5;208mError!\33[0m", file, fnc, line, msg,
-               expr);
+  (void)printf(ERR_STR, (type) ? "\33[1m\33[38;5;196mFatal Error!\33[0m" : "\33[1m\33[38;5;208mError!\33[0m", file, fnc,
+               line, msg, expr);
   (void)printf("\33[38;5;196mAborting Now!\33[0m\n");
   abort();
 }
